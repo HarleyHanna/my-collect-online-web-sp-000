2c96cfb3cf94dfd87f20c971aeffcb13fc04f7b0
Harley Hanna <harleyhannawrit@gmail.com>
2020-06-11 16:27:23 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
-
-
+def my_collect (collection)
+  i = 0
+  final_collection = []
+  if collection.length == 0
+      return nil
+  else
+    while i <= collection.length
+      final_collection << yield(collection[i])
+      i += 1
+    end
+  end
+  return final_collection
+end
